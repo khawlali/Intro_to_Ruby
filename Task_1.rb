f9752c6998bbda7a412ae5bd0732fdeb5a1c9352
khawlali <khawlaz_@hotmail.com>
2022-01-16 21:00:33 +0300

    done

--- Task_1.rb
+++ Task_1.rb
@@ -1,3 +1,32 @@
+s = "Hello Ruby World"
+s.delete_prefix!("Hello ")
+puts s
 
-p x = "HI" 
+s= "Welcome to " + s 
+puts s 
+
+s["World"]= "Khawla"
+puts s
+
+s.insert(16, "\"")
+s.insert(23, "\"")
+puts s
+
+s.insert(15, ",")
+puts s
+
+s.insert(16, "\t")
+puts s
+
+count_ch =s.count(s) 
+puts count_ch
+
+count_s =s.count(" ") 
+puts count_s
+
+s= s.gsub(" ", "-")
+puts s
+
+
+
 
